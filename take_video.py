d7a4ed53fd83999480c827a76b1389e9719d86d7
JakubKubista <xkubista@gmail.com>
2019-03-29 15:58:36 +0100

    Change camera setup

--- take_video.py
+++ take_video.py
@@ -1,20 +1,34 @@
-# import the necessary packages
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import time
+# OpenCV Python program to detect cars in video frame
+# import libraries of python OpenCV
 import cv2
 
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-rawCapture = PiRGBArray(camera)
+# capture frames from a video
+cap = cv2.VideoCapture('video.avi')
 
-# allow the camera to warmup
-time.sleep(0.1)
+# Trained XML classifiers describes some features of some object we want to detect
+car_cascade = cv2.CascadeClassifier('cars.xml')
 
-# grab an image from the camera
-camera.capture(rawCapture, format="bgr")
-image = rawCapture.array
+# loop runs if capturing has been initialized.
+while True:
+    # reads frames from a video
+    ret, frames = cap.read()
 
-# display the image on screen and wait for a keypress
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+    # convert to gray scale of each frames
+    gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
+
+    # Detects cars of different sizes in the input image
+    cars = car_cascade.detectMultiScale(gray, 1.1, 1)
+
+    # To draw a rectangle in each cars
+    for (x, y, w, h) in cars:
+        cv2.rectangle(frames, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+        # Display frames in a window
+cv2.imshow('video2', frames)
+
+# Wait for Esc key to stop
+if cv2.waitKey(33) == 27:
+    break
+
+# De-allocate any associated memory usage
+cv2.destroyAllWindows()
